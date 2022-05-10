0540784ad223ccd2ca41c290929e413bad063e6d
ReedmByford <reedbyford@gmail.com>
2022-05-10 00:18:06 -0500

    Edit README

--- assets/js/main.js
+++ assets/js/main.js
@@ -784,3 +784,9 @@ fetch('https://uselessfacts.jsph.pl/random.json')
 					});
 
 })(jQuery);
+
+/*
+	Ethereal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
